b568af47caeeaf7762574e1dc5ad688591a606eb
zhoushuo19850509 <zhoushuo19850509@163.com>
2023-08-19 10:21:08 +0800

    add UML

--- src/com/Test.java
+++ src/com/Test.java
@@ -1,7 +1,21 @@
 package com;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args){
+
+        System.out.println("hello ...");
+
+        Scanner in = new Scanner(System.in);
+
+        while(in.hasNext()){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            System.out.println( a + b);
+
+        }
+
 
     }
 }
